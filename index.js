d949266c94d0187b4875c93a52e9976d44b91682
Rufai Sanni <sruffy07@gmail.com>
2023-08-14 12:55:51 +0100

    first commit to first-budget-app1

---
+++ index.js
@@ -0,0 +1,32 @@
+let totalAmount = document.getElementById("total-amount");
+let userAmount = document.getElementById("user-amount");
+const checkAmountButton = document.getElementById("check-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
+const productTitle = document.getElementById("product-title");
+const errorMessage = document.getElementById("budget-error");
+const productTitleError = document.getElementById("product-title-error");
+const productCostError = document.getElementById("product-cost-error");
+const amount = document.getElementById("amount");
+const expenditureValue = document.getElementById("expenditure-value");
+const balanceValue = document.getElementById("balance-amount");
+const list = document.getElementById("list");
+let tempAmount = 0;
+
+// Set the Budget part
+totalAmountButton.addEventListener("click", function(){
+    tempAmount = totalAmount.value;
+
+    // Empty or negative input
+    if(tempAmount ==="" || tempAmount < 0){
+        errorMessage.classList.remove("hide");
+    } else {
+        errorMessage.classList.add("hide");
+        // Set budget
+        amount.innerHTML = tempAmount;
+        // Set balance
+        balanceValue.innerText = tempAmount - expenditureValue.innerText;
+        // CLear input box
+        totalAmount.value = "";
+    }
+});
+
